8ca98adbde8ee4ae64697a26ab5cdcbed1895439
Javier <devjavi@MacBook-Air-de-Javier.local>
2026-05-30 23:00:51 -0500

    deploy

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,4 +1,4 @@
-import { createHashRouter, Navigate } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import MainLayout from "@/layouts/MainLayout";
 import Home from "@/pages/Home/Home";
 import Projects from "@/pages/Projects/Projects";
@@ -6,7 +6,7 @@ import About from "@/pages/About/About";
 import Me from "@/pages/Me/Me";
 import NotFound from "@/pages/NotFound/NotFound";
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
   {
     element: <MainLayout />,
     children: [
@@ -14,9 +14,12 @@ export const router = createHashRouter([
       { path: "/projects", element: <Projects /> },
       { path: "/about", element: <About /> },
       { path: "/me", element: <Me /> },
+
+      // redirects (manteniendo compatibilidad)
       { path: "/Projects", element: <Navigate to="/projects" replace /> },
       { path: "/About", element: <Navigate to="/about" replace /> },
       { path: "/Me", element: <Navigate to="/me" replace /> },
+
       { path: "*", element: <NotFound /> },
     ],
   },
