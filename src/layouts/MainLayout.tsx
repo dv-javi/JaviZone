fbf65b9857c307b63eae671e9c032f35c8042aca
Javier <devjavi@MacBook-Air-de-Javier.local>
2026-05-31 10:04:43 -0500

    deploy

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { useEffect } from "react";
-import Galaxy from "@/components/ui/Backgrounds/Space/Galaxy";
 import Navbar from "@/components/layout/Navbar/Navbar";
 import FeedbackModal from "@/components/layout/FeedbackModal/FeedbackModal";
 import { useLenis } from "@/hooks/useLenis";
@@ -17,24 +16,7 @@ function ScrollToTop() {
 }
 
 function SpaceBackground() {
-  return (
-    <div className="bg-space">
-      <Galaxy
-        mouseRepulsion
-        mouseInteraction
-        density={1}
-        glowIntensity={0.2}
-        saturation={0}
-        hueShift={120}
-        twinkleIntensity={0.3}
-        rotationSpeed={0.1}
-        repulsionStrength={1}
-        autoCenterRepulsion={0}
-        starSpeed={0.2}
-        speed={0.5}
-      />
-    </div>
-  );
+  return <div className="bg-space"></div>;
 }
 
 export default function MainLayout() {
