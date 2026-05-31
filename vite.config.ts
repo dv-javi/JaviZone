3fa6b6b63c5c44e4feaf737fd5f860c6a4dd1b30
Javier <devjavi@MacBook-Air-de-Javier.local>
2026-05-31 09:46:32 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
-import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      open: true,
-      gzipSize: true,
-      filename: "stats.html",
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
